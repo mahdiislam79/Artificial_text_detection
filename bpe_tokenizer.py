82ba17706da5ce16b046452a74750a47113d8954
Mahdi Islam <84149578+mahdiislam79@users.noreply.github.com>
2024-01-04 02:48:01 +0100

    editing bpe_tokenizer and train.py

--- bpe_tokenizer.py
+++ bpe_tokenizer.py
@@ -1,5 +1,4 @@
 # bpe_tokenizer_module.py
-
 from tokenizers import (
     decoders,
     models,
@@ -14,41 +13,42 @@ from tqdm.auto import tqdm
 from datasets import Dataset
 
 class BPETokenizer:
-    special_tokens = ["[UNK]", "[PAD]", "[CLS]", "[SEP]", "[MASK]"]
-    
+    ST = ["[UNK]", "[PAD]", "[CLS]", "[SEP]", "[MASK]"]
     
     def __init__(self, vocab_size):
         self.vocab_size = vocab_size
-        self.tokenizer = Tokenizer(models.BPE(unk_token="[UNK]"))
-        self.tokenizer.normalizer = normalizers.Sequence([normalizers.NFC()])
-        self.tokenizer.pretokenizer = pre_tokenizers.ByteLevel()
-        self.tokenizer.post_processor = processors.TemplateProcessing(
-                                                                      single="[CLS] $A",
-                                                                      special_tokens=[("[CLS]", 1)])
+        self.tok = Tokenizer(models.BPE(unk_token="[UNK]"))
+        self.tok.normalizer = normalizers.Sequence([normalizers.NFC()])
+        self.tok.pre_tokenizer = pre_tokenizers.ByteLevel()
+        self.tok.post_processor = processors.TemplateProcessing(
+            single="[CLS] $A",
+            special_tokens=[("[CLS]", 1)],
+        )
+        
     @classmethod
-    def dataset_iterator(cls, dataset, chunk_size=1000):
-        for i in range(0, len(dataset), 1000):
-            yield dataset[i : i + 1000]["text"]
-
-    def train(self, df):
-        trainer = trainers.BpeTrainer(vocab_size=self.vocab_size, special_tokens=self.special_tokens)
-        dataset = Dataset.from_pandas(df[['text']])
-        self.tokenizer.train_from_iterator(self.dataset_iterator(dataset), trainer=trainer)
+    def chunk_dataset(cls, dataset, chunk_size=1_000):
+        for i in range(0, len(dataset), chunk_size):
+            yield dataset[i : i + chunk_size]["text"]
+        
+    def train(self, data):
+        trainer = trainers.BpeTrainer(vocab_size=self.vocab_size, special_tokens=self.ST)
+        dataset = Dataset.from_pandas(data[["text"]])
+        self.tok.train_from_iterator(self.chunk_dataset(dataset), trainer=trainer)
         return self
-
-    def encode(self, df):
+    
+    def tokenize(self, data):
         tokenized_texts = []
-        for text in tqdm(df['text'].tolist()):
-            tokenized_texts.append(self.tokenizer.encode(text))
+        for text in tqdm(data['text'].tolist()):
+            tokenized_texts.append(self.tok.encode(text))
         return tokenized_texts
-
-    def get_fast_tokenizer(self):
-        tokenizer = PreTrainedTokenizerFast(
-            tokenizer_object=self.tokenizer,
+    
+    def get_fast_tokenizer(self, max_length):
+        return PreTrainedTokenizerFast(
+            tokenizer_object=self.tok,
             unk_token="[UNK]",
             pad_token="[PAD]",
             cls_token="[CLS]",
             sep_token="[SEP]",
             mask_token="[MASK]",
+            model_max_length=max_length
         )
-        return tokenizer
